98e0e480722dcc7f3ab750ce33e2e7c7cd76122e
lve <lve.gh@outlook.com>
2024-03-04 21:56:13 +0300

    trying to fix ubuntu error

--- hw1_4/hw1_4/obj/Debug/net6.0/hw1_4.AssemblyInfo.fs
+++ hw1_4/hw1_4/obj/Debug/net6.0/hw1_4.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("hw1_4")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("Hw1_4")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("hw1_4")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("hw1_4")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("Hw1_4")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("Hw1_4")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
