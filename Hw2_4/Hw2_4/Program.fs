b5b92781f7bb27d17cb29256861dd8800fbb8b50
lve <lve.gh@outlook.com>
2024-03-12 15:11:54 +0300

    added homework2

---
+++ Hw2_4/Hw2_4/Program.fs
@@ -0,0 +1,51 @@
+﻿// 1
+let evenCountMap list =
+    list 
+    |> List.map (fun x -> if x % 2 = 0 then 1 else 0)
+    |> List.sum
+
+let evenCountFilter list =
+    list
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.length
+
+let evenCountFold list =
+    list
+    |> List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
+// 2
+module Tree =
+    type 'a Tree =
+        | Empty
+        | Node of 'a * 'a Tree * 'a Tree
+
+    let rec map f =
+        function
+        | Empty -> Empty
+        | Node(value, left, right) -> Node(f value, map f left, map f right)
+// 3
+type Expr =
+    | Const of int
+    | Add of Expr * Expr
+    | Mul of Expr * Expr
+
+let rec eval expr =
+    match expr with
+    | Const c -> c
+    | Add (e1, e2) -> eval e1 + eval e2
+    | Mul (e1, e2) -> eval e1 * eval e2
+// 4
+//let rec primes = Seq.unfold (fun state -> Some(state, state + 1)) 2
+//               |> Seq.filter (fun x -> Seq.forall (fun prime -> x % prime <> 0) (Seq.takeWhile (fun y -> y * y <= x) primes))
+let rec isPrime n =
+    let upperBound = int(sqrt(float n))
+    let rec checkDivisor divisor =
+        divisor > upperBound || (n % divisor <> 0 && checkDivisor (divisor + 1))
+    checkDivisor 2
+
+let primes = Seq.unfold (fun state -> Some(state, state + 1)) 2
+             |> Seq.filter isPrime
+
+let actualPrimes = primes |> Seq.take 10 |> Seq.toList
+//primes |> Seq.iter (printfn "%d")
+for item in actualPrimes do
+    printfn "%d" item
---
+++ Hw2_4/Program.tests/UnitTest1.fs
@@ -0,0 +1,50 @@
+module Program.tests
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+open Tree
+//1
+[<Test>]
+let MapEqualsFilter () =
+    Check.QuickThrowOnFailure(fun l -> evenCountMap l = evenCountFilter l)
+
+[<Test>]
+let MapEqualsFold () =
+    Check.QuickThrowOnFailure(fun l -> evenCountMap l = evenCountFold l)
+
+[<Test>]
+let FoldEqualsFilter () =
+    Check.QuickThrowOnFailure(fun l -> evenCountFold l = evenCountFilter l)
+
+//2
+[<Test>]
+let ``Should return the expected tree``() =
+    let inputTree = Node(1, Node(2, Empty, Node(3, Empty, Empty)), Empty)
+    let expectedTree = Node(2, Node(4, Empty, Node(6, Empty, Empty)), Empty)
+    let actualTree = map (fun x -> x * 2) inputTree
+    actualTree |> should equal expectedTree
+
+//3
+[<Test>]
+let ``Should return correct result``() =
+    let exprTree = Add(Const(2), Mul(Const(3), Const(4)))
+    let result = eval exprTree
+    result |> should equal 14
+//4
+[<Test>]
+let ``Lists should be equal``() =
+    let expectedPrimes = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29] 
+
+    let rec isPrime n =
+        let upperBound = int(sqrt(float n))
+        let rec checkDivisor divisor =
+            divisor > upperBound || (n % divisor <> 0 && checkDivisor (divisor + 1))
+        checkDivisor 2
+
+    let primes = Seq.unfold (fun state -> Some(state, state + 1)) 2
+                |> Seq.filter isPrime
+
+    let actualPrimes = primes |> Seq.take 10 |> Seq.toList
+    //let actualPrimes = primes |> Seq.take 10 |> Seq.toList 
+    actualPrimes |> should equal expectedPrimes
