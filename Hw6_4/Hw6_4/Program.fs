732a52def92d00a19b558521636d548dbfc3e1ed
lve-gh <lve.github@outlook.com>
2024-04-25 19:59:48 +0300

    added hw6

---
+++ Hw6_4/Hw6_4/Program.fs
@@ -0,0 +1,14 @@
+﻿module Hw6_4
+
+open System
+
+type WorkflowRound(accuracy : int) =
+    member this.Bind (x : float, f) = f x
+    member this.Return (x : float) = Math.Round(x, accuracy)
+
+type WorkflowStrings() =
+    member this.Bind (x : string, f) =
+        match Int32.TryParse x with
+        | true, y -> f y
+        | false, y -> None
+    member this.Return x = Some x
