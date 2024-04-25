4381a8bd848cc97d4b71023f84bd0b66a7802c41
lve-gh <lve.github@outlook.com>
2024-04-25 20:07:21 +0300

    fix

--- Hw2_4/Hw2_4/Program.fs
+++ Hw2_4/Hw2_4/Program.fs
@@ -12,6 +12,8 @@ let evenCountFilter list =
 let evenCountFold list =
     list
     |> List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
+
+
 // 2
 module Tree =
     type 'a Tree =
@@ -22,20 +24,26 @@ module Tree =
         function
         | Empty -> Empty
         | Node(value, left, right) -> Node(f value, map f left, map f right)
+
+
 // 3
 type Expr =
     | Const of int
     | Add of Expr * Expr
     | Mul of Expr * Expr
+    | Sub of Expr * Expr
+    | Div of Expr * Expr
 
 let rec eval expr =
     match expr with
     | Const c -> c
     | Add (e1, e2) -> eval e1 + eval e2
     | Mul (e1, e2) -> eval e1 * eval e2
+    | Sub (e1, e2) -> eval e1 - eval e2
+    | Div (e1, e2) -> eval e1 / eval e2
+
+
 // 4
-//let rec primes = Seq.unfold (fun state -> Some(state, state + 1)) 2
-//               |> Seq.filter (fun x -> Seq.forall (fun prime -> x % prime <> 0) (Seq.takeWhile (fun y -> y * y <= x) primes))
 let rec isPrime n =
     let upperBound = int(sqrt(float n))
     let rec checkDivisor divisor =
@@ -46,6 +54,5 @@ let primes = Seq.unfold (fun state -> Some(state, state + 1)) 2
              |> Seq.filter isPrime
 
 let actualPrimes = primes |> Seq.take 10 |> Seq.toList
-//primes |> Seq.iter (printfn "%d")
 for item in actualPrimes do
     printfn "%d" item
