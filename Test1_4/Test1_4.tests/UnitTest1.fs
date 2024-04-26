38846f497109a5ae00df046b33a89594160b6407
lve-gh <160332675+lve-gh@users.noreply.github.com>
2024-04-26 16:37:14 +0300

    Update UnitTest1.fs

--- Test1_4/Test1_4.tests/UnitTest1.fs
+++ Test1_4/Test1_4.tests/UnitTest1.fs
@@ -2,10 +2,23 @@ module Test1_4.tests
 
 open NUnit.Framework
 
-[<SetUp>]
-let Setup () =
-    ()
+[<Test>]
+let ``sumOfEvenFibonacci is correct`` () =
+    sumOfEvenFibonacci |> should equal 1089154
+
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let ``Dequeue throw exception`` () =
+    let queue = PriorityQueue<int>()
+    shouldThrow<InvalidOperationException> (fun () -> queue.Dequeue())
+
+[<Test>]
+let ``Enqueue и Dequeue is correct`` () =
+    let queue = PriorityQueue<string>()
+    queue.Enqueue("1", 5)
+    queue.Enqueue("2", 10)
+    queue.Enqueue("3", 1)
+    
+    queue.Dequeue() |> should equal "2" 
+    queue.Dequeue() |> should equal "1"
+    queue.Dequeue() |> should equal "3" 
