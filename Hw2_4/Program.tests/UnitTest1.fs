332f8f85c4981aba99bf3ea3a7646ee05e8d4ba3
lve-gh <lve.github@outlook.com>
2024-05-31 17:01:27 +0300

    fix

--- Hw2_4/Program.tests/UnitTest1.fs
+++ Hw2_4/Program.tests/UnitTest1.fs
@@ -25,12 +25,21 @@ let ``Should return the expected tree``() =
     let actualTree = map (fun x -> x * 2) inputTree
     actualTree |> should equal expectedTree
 
+[<Test>]
+let ``Non-degenerate tree``() =
+    let inputTree = Node(1, Node(2, Node(3, Empty, Empty), Node(4, Empty, Empty) ), Node(5, Node(6, Empty, Empty), Node(7, Empty, Empty)))
+    let expectedTree = Node(2, Node(4, Node(6, Empty, Empty), Node(8, Empty, Empty) ), Node(10, Node(12, Empty, Empty), Node(14, Empty, Empty)))
+    let actualTree = map (fun x -> x * 2) inputTree
+    actualTree |> should equal expectedTree
+
 //3
 [<Test>]
 let ``Should return correct result``() =
-    let exprTree = Add(Const(2), Mul(Const(3), Const(4)))
+    let exprTree = Binary(Add, Const(2), Binary(Mul, Const(3), Const(4)))
     let result = eval exprTree
     result |> should equal 14
+
+
 //4
 [<Test>]
 let ``Lists should be equal``() =
